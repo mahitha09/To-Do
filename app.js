74bd4949ee63bea68c984c847b463f861e3c993a
mahitha09 <mahitha.yellani@gmail.com>
2021-05-12 12:52:41 -0400

    Update

--- app.js
+++ app.js
@@ -1,28 +1,14 @@
 //variables
-const todoInput = document.querySelector('.todo-input');
-const addBtn = document.querySelector('.add-btn');
-const todoContainer = document.querySelector('.todo-container');
+const form = document.getElementById('form');
+const input = document.getElementById('todoinput');
+const todos = document.getElementById('todos');
 
-//eventlisteners
-addBtn.addEventListener('click',addTodo);
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
 
-//function
-function addTodo(){
-    const todoDiv = document.createElement('div');
-    todoDiv.classList.add('todo-div');
-    //create list
-    const newTodo = document.createElement('li');
-    newTodo.innerText = "Hii";
-    newTodo.classList.add('todo-list');
-    todoDiv.appendChild(newTodo);
-    //check mark
-    const completedButton = document.createElement('button');
-    completedButton.classList.add('completed-btn');
-    completedButton.innerHTML = '<i class = "fa fa-check"></i>';
-    todoDiv.appendChild(completedButton);
-    //delete button
-    const deleteButton = document.createElement('button');
-    deleteButton.classList.add('delete-btn');
-    deleteButton.innerHTML = '<i class = "fa fa-trash"></i>';
-    todoDiv.appendChild(deleteButton);
-}
+    const todoli = document.createElement('li');
+    todoli.innerText = input.value;
+    todos.appendChild(todoli);
+
+    input.value = "";
+});
