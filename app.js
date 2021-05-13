44b0a334b4bc7ed1330698e035d71a9e8b0d0186
mahitha09 <mahitha.yellani@gmail.com>
2021-05-13 11:02:55 -0400

    Update

--- app.js
+++ app.js
@@ -7,31 +7,67 @@ const btnInput = document.getElementById('btn-input');
 let todos = JSON.parse(localStorage.getItem('todos'));
 
 if(todos){
-    todos.forEach(t => {
-        addToDo(t);
+    todos.forEach(todo => {
+        let todoText = input.value;
+
+        if(todo){
+            todoText = todo.text;
+        }
+    
+        if(todoText){
+            const todoli = document.createElement('li');
+    
+            if(todo.completed){
+                todoli.classList.add('completed');
+            }
+    
+            todoli.innerText = todoText;
+    
+            //creating a div
+            const todoDiv = document.createElement('div');
+    
+            //creating buttons
+            const checkbtn = document.createElement('button');
+            checkbtn.innerHTML = '<i class="fa fa-check"></i>';
+            checkbtn.classList.add('check-btn');
+            const deletebtn = document.createElement('button');
+            deletebtn.innerHTML = '<i class="fa fa-trash"></i>';
+            deletebtn.classList.add('delete-btn');
+    
+            //appending buttons to div
+            todoDiv.appendChild(checkbtn);
+            todoDiv.appendChild(deletebtn);
+    
+            //appending div to li
+            todoli.appendChild(todoDiv);
+            todoUl.appendChild(todoli);
+    
+            //Activating Check button
+            
+            checkbtn.addEventListener('click',(e)=>{
+                e.preventDefault();
+                todoli.classList.toggle('completed');
+                updateLocalStorage();
+            });
+    
+            //Activating Delete Button
+    
+            deletebtn.addEventListener('click',()=>{
+                todoli.remove();
+                updateLocalStorage();
+            });
+        
+        input.value = "";
+        updateLocalStorage();
+        }
     });
 }
+
 btnInput.addEventListener('click',(e) => {
     e.preventDefault();
-    addToDo();
-});
-
-
-function addToDo(todo)
-{
     let todoText = input.value;
-
-    if(todo){
-        todoText = todo.text;
-    }
-
     if(todoText){
         const todoli = document.createElement('li');
-
-        if(todo.completed){
-            todoli.classList.add('completed');
-        }
-
         todoli.innerText = todoText;
 
         //creating a div
@@ -72,7 +108,9 @@ function addToDo(todo)
 
     updateLocalStorage();
     }
-}
+});
+
+//Creting the local storage function
 
 function updateLocalStorage(){
     const todosli = document.querySelectorAll('li');
